0fee4d33b7144fbbc10151b1aaf1278ac4d660b7
정준혁 <wings9701jh@gmail.com>
2022-11-18 15:40:50 +0900

    first commit

---
+++ gitignoretest/src/test/java/com/example/gitignoretest/GitignoretestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitignoretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitignoretestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
